2f565c938d4ae98da68462bcc1157b1a383cb53c
5piffe <spiffe84@gmail.com>
2020-08-23 23:51:20 +0200

    Added unreal project

---
+++ Source/ShooterGame/Public/Pickups/ShooterPickup_Health.h
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "ShooterPickup.h"
+#include "ShooterPickup_Health.generated.h"
+
+class AShooterCharacter;
+
+// A pickup object that replenishes character health
+UCLASS(Abstract, Blueprintable)
+class AShooterPickup_Health : public AShooterPickup
+{
+	GENERATED_UCLASS_BODY()
+
+	/** check if pawn can use this pickup */
+		UFUNCTION(Category = Pickup, BlueprintCallable)
+	virtual bool CanBePickedUp(AShooterCharacter* TestPawn) const override;
+
+protected:
+
+	/** how much health does it give? */
+	UPROPERTY(EditDefaultsOnly, Category=Pickup)
+	int32 Health;
+
+	/** give pickup */
+	UFUNCTION(Category = Pickup, BlueprintCallable)
+	virtual void GivePickupTo(AShooterCharacter* Pawn) override;
+};
